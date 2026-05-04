2c2c197f64726f553bcb165255fece68cfdfcfb7
wingedseraph <r89647@gmail.com>
2026-05-04 21:40:43 +0300

    feat(main): wrap entire app into ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { App } from './App'
+import { ErrorBoundary } from '@/components/error-boundary/ErrorBoundary'
+
 import './index.css'
+import { App } from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
