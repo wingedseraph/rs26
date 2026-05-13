bff1cc7befa8112398a8af7e229eefddb22462d2
wingedseraph <r89647@gmail.com>
2026-05-13 12:27:23 +0300

    feat(router): add routing for about and error page

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import { ErrorBoundary } from '@/components/error-boundary/ErrorBoundary'
+import { RouterProvider } from 'react-router'
 
 import './index.css'
-import { App } from './App'
+import { ErrorBoundary } from '@/components/error-boundary/ErrorBoundary'
+import { router } from '@/router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </StrictMode>,
 )
---
+++ src/router.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from 'react-router'
+
+import { getQueryImages } from '@/api/api'
+import { App, STORAGE } from '@/App'
+
+const PATH = {
+  index: '/',
+  about: 'about',
+  error: '*',
+} as const
+
+export const router = createBrowserRouter([
+  {
+    path: PATH.index,
+    loader: async () => {
+      return { records: await getQueryImages(localStorage.getItem(STORAGE) ?? '') }
+    },
+    element: <App />,
+  },
+  {
+    path: PATH.about,
+    element: <h1 id='center'> about </h1>,
+  },
+  {
+    path: PATH.error,
+    element: <h1 id='center'> error </h1>,
+  },
+])
