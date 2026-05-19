b611d475183f76a611bc5cd9cc2f7674ea21e6c0
wingedseraph <r89647@gmail.com>
2026-05-19 17:35:56 +0300

    fix(router): redirect to ?page=1 on initial load

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react'
-import { createBrowserRouter } from 'react-router'
+import { createBrowserRouter, redirect } from 'react-router'
 import type { LoaderFunctionArgs } from 'react-router'
 
 import { getByIdArtwork, getByQueryArtwork } from '@/api/api'
@@ -23,9 +23,14 @@ export const routes
       {
         path: PATH.index,
         loader: async ({ request }: LoaderFunctionArgs) => {
+          // todo: упросить логику, будто можно сделать и лаконичнее
           const query = localStorage.getItem(STORAGE) ?? ''
           const parameters = new URL(request.url.toString()).searchParams
-          const page = Number(parameters.get('page')) || 1
+          if (parameters.get('page') == null) {
+            return redirect('?page=1')
+          }
+          const page = Number(parameters.get('page'))
+
           const nonNegativePage = page > 0 ? page : 1
 
           const result = await getByQueryArtwork(query, nonNegativePage)
