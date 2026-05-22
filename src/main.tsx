a7df2cd9e94c0b4b4b499f913e0c2e667e9c882b
wingedseraph <r89647@gmail.com>
2026-05-22 22:41:38 +0300

    feat(store): add selectedCards slice with toggleOne and removeAll

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router'
+import { Provider } from 'react-redux'
 
 import './index.css'
+import { RouterProvider } from 'react-router'
+
 import { router } from '@/router'
+import store from '@/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
