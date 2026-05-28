db4be4d7a6691935f9d9eb1b64945f926975af26
wingedseraph <r89647@gmail.com>
2026-05-28 19:48:12 +0300

    refactor(card-list): extract CardItem component

---
+++ src/components/card-list/Card.tsx
@@ -0,0 +1,57 @@
+import { Link } from 'react-router'
+
+import type { Card } from '@/api/typeguard'
+
+import { CardListFooter } from '@/components/card-list/CardListFooter'
+import { toggleOne, useAppDispatch, useAppSelector } from '@/store'
+
+type CardItemProperties = {
+  card: Card
+  page: number
+}
+
+function CardItem({ card, page }: CardItemProperties) {
+  const dispatch = useAppDispatch()
+  const selectedCards = useAppSelector(state => state.selectedCards)
+
+  return (
+    <div
+      className='
+        appear relative my-10 w-full cursor-pointer break-inside-avoid rounded-md-custom bg-white p-1 shadow-card
+        transition-shadow duration-200
+        hover:shadow-card-hover
+      '
+      key={card.systemNumber}
+      title={card._primaryTitle}
+    >
+      <Link viewTransition to={{ pathname: `card/${card.systemNumber}`, search: `page=${page}` }}>
+        <div className='flex flex-col gap-1 p-1'>
+          <div className='flex min-h-40 w-full cursor-default justify-center rounded-md-custom bg-stone-6/30'>
+            <img
+              className='
+                max-h-40 w-full cursor-zoom-in rounded-xs object-contain transition-opacity duration-150
+                hover:opacity-[0.92]
+              '
+              src={`${card._images._iiif_image_base_url}full/!600,600/0/default.jpg`}
+              alt={card.objectType}
+            />
+          </div>
+
+          <div className='flex items-center justify-center p-2'>
+            <h2 className='line-clamp-3 text-base-custom text-silver-font'>
+              {card._primaryTitle.length > 0 ? card._primaryTitle : card.objectType}
+            </h2>
+          </div>
+
+        </div>
+      </Link>
+
+      <CardListFooter
+        onClick={() => dispatch(toggleOne({ id: card.systemNumber, card }))}
+        isSelected={Object.hasOwn(selectedCards, card.systemNumber)}
+      />
+
+    </div>
+  )
+}
+export { CardItem }
--- src/components/card-list/CardList.tsx
+++ src/components/card-list/CardList.tsx
@@ -1,9 +1,9 @@
-import { Link, useOutlet } from 'react-router'
+import { useOutlet } from 'react-router'
 
 import type { Card } from '@/api/typeguard'
 
-import { CardListFooter } from '@/components/card-list/CardListFooter'
-import { toggleOne, useAppDispatch, useAppSelector } from '@/store'
+import { CardItem } from '@/components/card-list/Card'
+import { cn } from '@/lib/utilities'
 
 type CardListProperties = {
   data: Card[]
@@ -12,8 +12,6 @@ type CardListProperties = {
 
 function CardList({ data: cards, page }: CardListProperties) {
   const outlet = useOutlet()
-  const dispatch = useAppDispatch()
-  const selectedCards = useAppSelector(state => state.selectedCards)
 
   if (cards.length === 0) {
     return (
@@ -22,54 +20,9 @@ function CardList({ data: cards, page }: CardListProperties) {
   }
 
   return (
-    <div className={`
-      columns-2 gap-6 pt-10
-      ${outlet ? 'md:columns-2' : 'md:columns-3'}
-    `}
-    >
+    <div className={cn(`columns-2 gap-6 pt-10 md:columns-3`, { 'md:columns-2': outlet })}>
       {cards.map(element => (
-        <div
-          className='
-            appear relative my-10 w-full cursor-pointer break-inside-avoid
-            rounded-md-custom bg-white p-1 shadow-card transition-shadow
-            duration-200
-            hover:shadow-card-hover
-          '
-          key={element.systemNumber}
-          title={element._primaryTitle}
-        >
-          <Link viewTransition to={{ pathname: `card/${element.systemNumber}`, search: `page=${page}` }}>
-            <div className='flex flex-col gap-1 p-1'>
-              <div className='
-                flex min-h-40 w-full cursor-default justify-center bg-stone-6/30
-              '
-              >
-                <img
-                  className='
-                    max-h-40 w-full cursor-zoom-in rounded-xs object-contain
-                    transition-opacity duration-150
-                    hover:opacity-[0.92]
-                  '
-                  src={`${element._images._iiif_image_base_url}full/!600,600/0/default.jpg`}
-                  alt={element.objectType}
-                />
-              </div>
-
-              <div className='flex items-center justify-center p-2'>
-                <h2 className='line-clamp-3 text-base-custom text-silver-font'>
-                  {element._primaryTitle.length > 0 ? element._primaryTitle : element.objectType}
-                </h2>
-              </div>
-
-            </div>
-          </Link>
-
-          <CardListFooter
-            onClick={() => dispatch(toggleOne({ id: element.systemNumber, card: element }))}
-            isSelected={Object.hasOwn(selectedCards, element.systemNumber)}
-          />
-
-        </div>
+        <CardItem card={element} page={page} key={element.systemNumber} />
       ))}
     </div>
   )
