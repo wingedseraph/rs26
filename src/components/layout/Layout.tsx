d21d4a6f5f0388b2c07cf043e60a2621d7f4d5b9
wingedseraph <r89647@gmail.com>
2026-05-28 19:47:38 +0300

    refactor(layout): simplify with cn() and Spinner component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,25 +1,25 @@
-import { Outlet, useNavigation } from 'react-router'
+import { Outlet, useNavigation, useOutlet } from 'react-router'
 
 import { ErrorBoundary } from '@/components/error-boundary/ErrorBoundary'
 import { Spinner } from '@/components/ui/spinner'
+import { cn } from '@/lib/utilities'
+
+const baseStyle = 'flex items-center justify-center gap-20 mx-auto'
+const outletStyle = 'mx-0 w-screen flex-col md:mx-20 md:flex-row'
+export const baseStyleDetailed = 'transition-all duration-300 hidden'
+export const outletStyleDetailed = 'flex w-full items-center justify-center md:relative md:w-1/2'
 
 function Layout() {
+  const outlet = useOutlet()
   const navigation = useNavigation()
   const isNavigating = Boolean(navigation.location)
   return (
     <ErrorBoundary>
-      {isNavigating && (
-        <div className='appear fixed inset-0 z-50 backdrop-blur-md'>
-          <div className='
-            absolute top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2
-          '
-          >
-            <Spinner> Loading... </Spinner>
-          </div>
-        </div>
-      )}
+      {isNavigating && (<Spinner />)}
 
-      <Outlet />
+      <div className={cn(baseStyle, { [outletStyle]: outlet })}>
+        <Outlet />
+      </div>
     </ErrorBoundary>
   )
 }
