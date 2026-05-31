4bf6573ce6294c3f53d4c938bdcf7a8d3dd7ab7a
wingedseraph <r89647@gmail.com>
2026-05-31 23:39:33 +0300

    feat(header): add cache invalidation buttons, extract link styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,10 +2,19 @@ import { useState } from 'react'
 import type { ChangeEvent, SyntheticEvent } from 'react'
 import { Link, useNavigate, useOutlet, useSearchParams } from 'react-router'
 
+import { artworkApi, byIdTag, byQueryTag } from '@/api/services/artwork'
 import { CombinedInput } from '@/components/combined-input/CombinedInput'
 import { useLocalStorage } from '@/hooks/useLocalStorage'
 import { useTheme } from '@/hooks/useTheme'
+import { cn } from '@/lib/utilities'
 import { PATH } from '@/router'
+import { useAppDispatch } from '@/store'
+
+const baseStyle = `
+  text-header-about absolute right-0 z-10 rounded-xl p-1 text-stone-5 outline-hidden transition-colors
+  hover:bg-stone-6
+  focus-visible:ring-1 focus-visible:ring-black
+`
 
 function Header() {
   const store = useLocalStorage('')
@@ -15,11 +24,13 @@ function Header() {
     setValue(event_.target.value)
   }
 
+  // todo: rewrite as custom hook to take page
   const [searchParameters] = useSearchParams()
   const pageParameters = Number(searchParameters.get('page'))
   const theme = useTheme()
   const outlet = useOutlet()
   const navigate = useNavigate()
+  const dispatch = useAppDispatch()
 
   const onSubmit = (event_: SyntheticEvent) => {
     event_.preventDefault()
@@ -32,59 +43,27 @@ function Header() {
       onSubmit={onSubmit}
       className='flex w-full flex-col items-center justify-between pt-4 pb-2 font-sans'
     >
-      <h1>
-        An inspiration engine for ideas
-      </h1>
-
-      <Link
-        viewTransition
-        to={PATH.about}
-        className='
-          text-header-about absolute top-0 right-0 rounded-xl p-1 text-stone-5 outline-hidden transition-colors
-          hover:bg-stone-6
-          focus-visible:ring-1 focus-visible:ring-black
-        '
-      >
-        about
-      </Link>
-
-      <Link
-        viewTransition
-        to={PATH.error}
-        className='
-          text-header-about absolute top-8 right-0 rounded-xl p-1 text-stone-5 outline-hidden transition-colors
-          hover:bg-stone-6
-          focus-visible:ring-1 focus-visible:ring-black
-        '
-      >
-        error
-      </Link>
+      <h1> An inspiration engine for ideas </h1>
+      <Link viewTransition to={PATH.about} className={cn(baseStyle, 'top-0')}> about </Link>
+      <Link viewTransition to={PATH.error} className={cn(baseStyle, 'top-8')}> error </Link>
 
       <button
         type='button'
         onClick={() => theme.setTheme(theme.value === 'light' ? 'dark' : 'light')}
-        className='
-          text-header-about absolute top-16 right-0 rounded-xl p-1 text-stone-5 outline-hidden transition-colors
-          hover:bg-stone-6
-          focus-visible:ring-1 focus-visible:ring-black
-        '
+        className={cn(baseStyle, `top-16`)}
       >
         {theme.value === 'light' ? 'dark' : 'light'}
       </button>
+
+      <button className={cn(baseStyle, 'top-24')} onClick={() => dispatch(artworkApi.util.invalidateTags([byQueryTag]))}> revalidate getByQuery cache </button>
+      <button className={cn(baseStyle, 'top-32')} onClick={() => dispatch(artworkApi.util.invalidateTags([byIdTag]))}> revalidate getById cache </button>
 
       {outlet
         && (
           <Link
             viewTransition
-            to={{
-              pathname: PATH.index,
-              search: `page=${pageParameters}`,
-            }}
-            className='
-              text-header-about absolute top-24 right-0 rounded-xl p-1 text-stone-5 outline-hidden transition-colors
-              hover:bg-stone-6
-              focus-visible:ring-1 focus-visible:ring-black
-            '
+            to={{ pathname: PATH.index, search: `page=${pageParameters}` }}
+            className={cn(baseStyle, `top-40`)}
           >
             close outlet
           </Link>
