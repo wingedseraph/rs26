da9566c9bc4dcdec1ca6cb4d780964bf0678bf2d
wingedseraph <r89647@gmail.com>
2026-06-02 00:26:39 +0300

    refactor(card-list): replace useOutlet with outlet css variant

--- src/components/card-list/CardList.tsx
+++ src/components/card-list/CardList.tsx
@@ -1,9 +1,6 @@
-import { useOutlet } from 'react-router'
-
 import type { Card } from '@/api/typeguard'
 
 import { CardItem } from '@/components/card-list/Card'
-import { cn } from '@/lib/utilities'
 
 type CardListProperties = {
   data: Card[]
@@ -11,8 +8,6 @@ type CardListProperties = {
 }
 
 function CardList({ data: cards, page }: CardListProperties) {
-  const outlet = useOutlet()
-
   if (cards.length === 0) {
     return (
       <h2 className='appear'>Oh No Data</h2>
@@ -20,7 +15,7 @@ function CardList({ data: cards, page }: CardListProperties) {
   }
 
   return (
-    <div className={cn(`columns-2 gap-6 pt-10 md:columns-3`, { 'md:columns-2': outlet })}>
+    <div className='columns-2 gap-6 pt-10 md:columns-3 outlet:md:columns-2'>
       {cards.map(element => (
         <CardItem card={element} page={page} key={element.systemNumber} />
       ))}
