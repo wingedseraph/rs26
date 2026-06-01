edd540c1e9a46a6e15fc6c4ffa756f8db5f5d3ff
wingedseraph <r89647@gmail.com>
2026-06-01 19:29:28 +0300

    test(mocks): add error and invalid id handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,14 +3,27 @@ import { delay, http, HttpResponse } from 'msw'
 import { BASE } from '@/api/api'
 import { MSW_SEARCH_RESPONSE, MSW_SINGLE_CARD_RESPONSE } from '@/mocks/mocks'
 
+export const VALID_ID = 1
+export const INVALID_ID = 4
+export const ERROR_ID = 999
+
 export const handlers = [
   http.get(`${BASE}/objects/search`, async () => {
     await delay(100)
     return HttpResponse.json(MSW_SEARCH_RESPONSE)
   }),
 
-  http.get(`${BASE}/museumobject/:id`, async () => {
+  http.get(`${BASE}/museumobject/${VALID_ID}`, async () => {
     await delay(100)
     return HttpResponse.json(MSW_SINGLE_CARD_RESPONSE)
+  }),
+
+  http.get(`${BASE}/museumobject/${INVALID_ID}`, async () => {
+    await delay(100)
+    return HttpResponse.json(null)
+  }),
+
+  http.get(`${BASE}/museumobject/${ERROR_ID}`, async () => {
+    return HttpResponse.error()
   }),
 ]
