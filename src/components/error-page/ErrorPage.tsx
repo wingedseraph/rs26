6e70e697e27f41d7fb14329b63e060cd9e373df3
wingedseraph <r89647@gmail.com>
2026-06-01 19:29:11 +0300

    refactor(error-page): add children prop for custom actions

--- src/components/error-page/ErrorPage.tsx
+++ src/components/error-page/ErrorPage.tsx
@@ -1,10 +1,13 @@
+import type { ReactNode } from 'react'
+
 import { BackLink } from '@/components/back-link/BackLink'
 
 type ErrorPageProperties = {
   errorMessage?: string
+  children?: ReactNode
 }
 
-export default function ErrorPage({ errorMessage }: ErrorPageProperties) {
+export default function ErrorPage({ errorMessage, children }: ErrorPageProperties) {
   return (
     <>
       <BackLink />
@@ -15,6 +18,7 @@ export default function ErrorPage({ errorMessage }: ErrorPageProperties) {
       >
         <h1 className='sm:text-8xl/30'>Something went wrong</h1>
         {errorMessage && <span>{errorMessage}</span>}
+        {children}
       </div>
     </>
   )
