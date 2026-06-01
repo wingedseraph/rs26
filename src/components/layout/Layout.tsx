ba4329475bdd36ae76cf1e778e29292540db05b9
wingedseraph <r89647@gmail.com>
2026-06-02 00:26:23 +0300

    refactor(layout): remove useOutlet, simplify base styles

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,13 @@
-import { Outlet, useLocation, useNavigation, useOutlet } from 'react-router'
+import { Outlet, useLocation, useNavigation } from 'react-router'
 
 import { ErrorBoundary } from '@/components/error-boundary/ErrorBoundary'
 import { Spinner } from '@/components/ui/spinner'
-import { cn } from '@/lib/utilities'
 
-const baseStyle = 'mx-auto flex items-center justify-center gap-20'
-const outletStyle = 'mx-0 w-screen flex-col md:mx-20 md:flex-row'
+const baseStyle = 'mx-auto flex w-screen flex-col items-center justify-center gap-20 md:mx-20 md:flex-row'
 export const baseStyleDetailed = 'hidden transition-all duration-300'
 export const outletStyleDetailed = 'flex w-full items-center justify-center md:relative md:w-1/2'
 
 function Layout() {
-  const outlet = useOutlet()
   const navigation = useNavigation()
   const location = useLocation()
   const isNavigating = Boolean(navigation.location)
@@ -18,7 +15,7 @@ function Layout() {
     <ErrorBoundary key={location.key}>
       {isNavigating && (<Spinner />)}
 
-      <div className={cn(baseStyle, { [outletStyle]: outlet })}>
+      <div className={baseStyle}>
         <Outlet />
       </div>
     </ErrorBoundary>
