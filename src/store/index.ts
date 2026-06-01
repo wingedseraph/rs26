e121c91b17ace9daf66ad6d2b02e6d4968db5c7f
wingedseraph <r89647@gmail.com>
2026-06-01 19:29:20 +0300

    refactor(store): extract setupStore factory for test isolation

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,31 @@
 import { useDispatch, useSelector } from 'react-redux'
 
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 import { artworkApi } from '@/api/services/artwork'
 import { selectedCards } from '@/store/slices/selectedCardsSlice'
 
-const store = configureStore({
-  reducer: {
-    selectedCards: selectedCards.reducer,
-    [artworkApi.reducerPath]: artworkApi.reducer,
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(artworkApi.middleware),
+const rootReducer = combineReducers({
+  selectedCards: selectedCards.reducer,
+  [artworkApi.reducerPath]: artworkApi.reducer,
 })
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export function setupStore(preloadedState?: PreloadedState) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware().concat(artworkApi.middleware),
+  })
+}
+
+const store = setupStore()
+
+export type PreloadedState = Parameters<typeof rootReducer>[0]
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = AppStore['dispatch']
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
 export const useAppSelector = useSelector.withTypes<RootState>()
 
