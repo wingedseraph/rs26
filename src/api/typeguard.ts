f189b3d0a256c090e134bac3186dbf832427260c
wingedseraph <r89647@gmail.com>
2026-06-02 22:33:51 +0300

    refactor(api): extract types to separate file

--- src/api/typeguard.ts
+++ src/api/typeguard.ts
@@ -1,24 +1,5 @@
-export type ValidResponse = {
-  records: Card[]
-  info: { record_count: number }
-}
-export type ValidResponseSingleCard = {
-  record: SingleCard
-}
-export type Card = {
-  systemNumber: number
-  objectType: string
-  _primaryTitle: string
-  _images: {
-    _iiif_image_base_url: string
-  }
-}
-export type SingleCard = {
-  systemNumber: number
-  dimensionsNote: string
-  briefDescription: string
-  images: [string]
-}
+import type { Card, SingleCard, ValidResponse, ValidResponseSingleCard } from '@/api/types'
+
 function isObject(argument: unknown): argument is object {
   return (argument !== null && typeof argument === 'object')
 }
