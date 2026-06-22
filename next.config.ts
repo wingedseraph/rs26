6374e4580d887d74fd7075eac3a17cf32ce42242
wingedseraph <r89647@gmail.com>
2026-06-23 00:39:09 +0300

    feat(i18n): add next-intl dependency and configure plugin

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from 'next'
 
+import createNextIntlPlugin from 'next-intl/plugin'
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -11,4 +13,9 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+const withNextIntl = createNextIntlPlugin({
+  experimental: {
+    createMessagesDeclaration: './messages/en.json',
+  },
+})
+export default withNextIntl(nextConfig)
