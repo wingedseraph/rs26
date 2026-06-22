b79075cace865d20f5a9ded8c703edce501a6ab6
wingedseraph <r89647@gmail.com>
2026-06-23 00:40:42 +0300

    feat(i18n): integrate i18n middleware into proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,22 @@
 import type { NextRequest } from 'next/server'
 
+import createMiddleware from 'next-intl/middleware'
 import { NextResponse } from 'next/server'
 
+import { routing } from '@/i18n/routing'
+
 export function proxy(request: NextRequest) {
-  if (!request.nextUrl.searchParams.has('page') && request.nextUrl.pathname === '/') {
+  const { searchParams, pathname } = request.nextUrl
+  const handleI18nRouting = createMiddleware(routing)
+  const response = handleI18nRouting(request)
+  const pathnameLocale = pathname.slice(1)
+
+  if (response.ok && !searchParams.has('page') && routing.locales.includes(pathnameLocale)) {
     request.nextUrl.searchParams.set('page', '1')
     return NextResponse.redirect(request.nextUrl)
   }
+
+  return response
 }
 
 export const config = {
