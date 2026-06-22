c2d7796e8c1c08be9d37306cf977e71fac5d8400
wingedseraph <r89647@gmail.com>
2026-06-23 01:31:12 +0300

    chore(hooks): remove unused usePage hook

--- src/hooks/usePage.ts
+++
@@ -1,9 +0,0 @@
-export function usePage() {
-  // fix: use params of next!
-  // const [searchParameters] = useSearchParams()
-  // const pageParameters = searchParameters.get('page') ?? '1'
-  // const page = Number(pageParameters) > 0 ? pageParameters : '1'
-
-  return '1'
-  // fix return page
-}
