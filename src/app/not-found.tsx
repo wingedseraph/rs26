fcdfca881e7d738d97b4ea3170aad5a5acdd7212
wingedseraph <r89647@gmail.com>
2026-06-23 01:20:15 +0300

    feat(i18n): add error, not-found and catch-all under [locale]

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,13 @@
-import { getTranslations } from 'next-intl/server'
+'use client'
 
-import { BackLink } from '@/components/ui/back-link'
+import Error from 'next/error'
 
-export default async function NotFoundPage() {
-  const t = await getTranslations('NotFoundPage')
-
+export default function RootNotFound() {
   return (
-    <>
-      <BackLink />
-
-      <div
-        id='center'
-        className='appear p-2 md:px-8 md:pt-6'
-      >
-        <h1 className='sm:text-8xl/30'>{t('title')}</h1>
-      </div>
-    </>
+    <html lang='en'>
+      <body>
+        <Error statusCode={404} />
+      </body>
+    </html>
   )
 }
